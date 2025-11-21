78bdce59d239ffc8a3adc598825b396bfce0cd6f
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-22 00:38:10 +0100

    just created the comp for the stud profile

---
+++ src/components/profilecard.jsx
@@ -0,0 +1,10 @@
+export function ProfileCard() {
+return (
+<Card className="w-full shadow-xl rounded-2xl p-5 mt-8">
+<h2 className="text-xl font-bold">My Profile</h2>
+<p className="text-gray-600 mt-2">Lendzele Fritz</p>
+<p className="text-gray-600">Computer Science Student</p>
+<Button className="mt-4 rounded-xl w-full">Edit Profile</Button>
+</Card>
+);
+}
